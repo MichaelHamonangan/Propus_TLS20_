b241a74bc4e1e9dbb4ceb51755e602833c4b99bf
MichaelHamonangan <72853893+MichaelHamonangan@users.noreply.github.com>
2020-10-19 16:46:20 +0700

    Add files via upload

---
+++ Propus_Adu Loncatan Jangkrik.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+   int x1,v1,x2,v2,i;
+   printf ("--------------------------------------------\n");
+   printf ("         Adu Loncatan Jangkrik\n");
+   printf ("--------------------------------------------\n");
+   printf ("Input x1 v1 x2 v2 (pisahkan dengan space) : ");
+   scanf ("%d %d %d %d",&x1,&v1,&x2,&v2);
+
+   if (x1<0)
+   printf ("\nError!!! Minimal nilai X1 adalah 0");
+    else if (x2<=x1)
+    printf ("\nError!!! X2 harus lebih besar dari X1");
+     else if (x2>1000)
+     printf ("\nError!!! Maksimal nilai X2 adalah 1000");
+      else if (v1<1)
+      printf ("\nError!!! Minimal nilai V1 adalah 1");
+        else if (v1>1000)
+        printf ("\nError!!! Maksimal nilai V1 adalah 1000");
+         else if (v2<1)
+         printf ("\nError!!! Minimal nilai V2 adalah 1");
+          else if (v2>1000)
+          printf ("\nError!!! Maksimal nilai V2 adalah 1000");
+            else
+            {
+                for (i=1;i<=1000;i++)
+                {
+                    x1+=v1;
+                    x2+=v2;
+                    if (x1==x2)
+                    {
+                        printf ("\nYES");
+                        break;
+                    }
+                }
+                if (x1!=x2)
+                    printf ("\nNO");
+            }      
+    
+    return 0;
+}
